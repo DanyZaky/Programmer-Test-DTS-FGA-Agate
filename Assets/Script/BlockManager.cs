426a538c20f29bac25ac47b0958018e75bee5e79
Dany Zaky <danyzakydany123@gmail.com>
2023-05-25 22:25:55 +0700

    block attack area

--- Assets/Script/BlockManager.cs
+++ Assets/Script/BlockManager.cs
@@ -5,7 +5,16 @@ using UnityEngine.UI;
 
 public class BlockManager : MonoBehaviour
 {
-    
+    public GameObject[] currentBlockPosition;
+
+    private void Start()
+    {
+        currentBlockPosition[0] = GameObject.FindWithTag("Bishop");
+        currentBlockPosition[1] = GameObject.FindWithTag("Dragon");
+        currentBlockPosition[2] = GameObject.FindWithTag("Knight");
+        currentBlockPosition[3] = GameObject.FindWithTag("Rock");
+    }
+
     public void onClickBlock()
     {
         BlockSprite.Instance.blockType = Random.Range(0, 3);
@@ -13,18 +22,42 @@ public class BlockManager : MonoBehaviour
         if (BlockSprite.Instance.blockType == 0)
         {
             BlockSprite.Instance.score += 2;
+            currentBlockPosition[0].GetComponent<RectTransform>().anchoredPosition = this.gameObject.GetComponent<RectTransform>().anchoredPosition;
+
+            currentBlockPosition[0].SetActive(true);
+            currentBlockPosition[1].SetActive(false);
+            currentBlockPosition[2].SetActive(false);
+            currentBlockPosition[3].SetActive(false);
         }
         else if (BlockSprite.Instance.blockType == 1)
         {
             BlockSprite.Instance.score += 1;
+            currentBlockPosition[1].GetComponent<RectTransform>().anchoredPosition = this.gameObject.GetComponent<RectTransform>().anchoredPosition;
+
+            currentBlockPosition[0].SetActive(false);
+            currentBlockPosition[1].SetActive(true);
+            currentBlockPosition[2].SetActive(false);
+            currentBlockPosition[3].SetActive(false);
         }
         else if (BlockSprite.Instance.blockType == 2)
         {
             BlockSprite.Instance.score += 1;
+            currentBlockPosition[2].GetComponent<RectTransform>().anchoredPosition = this.gameObject.GetComponent<RectTransform>().anchoredPosition;
+
+            currentBlockPosition[0].SetActive(false);
+            currentBlockPosition[1].SetActive(false);
+            currentBlockPosition[2].SetActive(true);
+            currentBlockPosition[3].SetActive(false);
         }
         else if (BlockSprite.Instance.blockType == 3)
         {
             BlockSprite.Instance.score += 2;
+            currentBlockPosition[3].GetComponent<RectTransform>().anchoredPosition = this.gameObject.GetComponent<RectTransform>().anchoredPosition;
+
+            currentBlockPosition[0].SetActive(false);
+            currentBlockPosition[1].SetActive(false);
+            currentBlockPosition[2].SetActive(false);
+            currentBlockPosition[3].SetActive(true);
         }
 
         this.gameObject.GetComponent<Image>().sprite = BlockSprite.Instance.currentBlockSprite;
